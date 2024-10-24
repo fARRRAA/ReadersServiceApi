6c6968454cad36e4b56e8236b06d91fc52266a7b
tksyksk <143901071+qmkvqw@users.noreply.github.com>
2024-10-24 22:21:05 +0300

    Добавьте файлы проекта.

---
+++ ReadersServiceApi/Controllers/ReaderController.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReadersServiceApi.dbContext;
+using ReadersServiceApi.Model;
+using ReadersServiceApi.Requests;
+using Microsoft.AspNetCore.Authorization;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using ReadersServiceApi.Interfaces;
+using System.Reflection.PortableExecutable;
+namespace LibraryWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReaderController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IReaderService _reader;
+        //private readonly IRentService _rent;
+        public ReaderController(IReaderService readerService/*, IRentService rent*/)
+        {
+            _reader = readerService;
+            //_rent = rent;
+        }
+
+        [HttpGet("getAllReaders")]
+        public async Task<ActionResult> GetAllReaders([FromQuery] int? page, [FromQuery] int? pageSize)
+        {
+
+            return new OkObjectResult(new
+            {
+                readers = _reader.GetAllReaders(page, pageSize)
+            });
+        }
+
+        [HttpPost("addNewReader")]
+        public async Task<IActionResult> AddNewReader([FromQuery]createReader reader)
+        {
+
+            if (_reader.ReaderExists(reader.Login))
+            {
+                return new NotFoundObjectResult(new
+                {
+                    error = NotFound("reader with that login and password already exists")
+                });
+            }
+            if (string.IsNullOrWhiteSpace(reader.Name) || string.IsNullOrWhiteSpace(reader.Password) || string.IsNullOrWhiteSpace(reader.Login) || string.IsNullOrWhiteSpace(reader.Date_Birth.ToString()))
+            {
+                return new BadRequestObjectResult(new
+                {
+                    error = BadRequest("fill in all fields")
+                });
+            }
+            await _reader.AddNewReader(reader);
+            return Ok();
+        }
+
+        [HttpPut("updateReaderById/{id}")]
+        public async Task<IActionResult> UpdateReaderById(int id, [FromQuery]createReader reader)
+        {
+
+            if (!_reader.GetAll().Any(r => r.Id_User == id))
+            {
+                return new NotFoundObjectResult(new
+                {
+                    error = NotFound("reader with that id does not exists")
+                });
+            }
+            if (string.IsNullOrWhiteSpace(reader.Name) || string.IsNullOrWhiteSpace(reader.Password) || string.IsNullOrWhiteSpace(reader.Login) || string.IsNullOrWhiteSpace(reader.Date_Birth.ToString()))
+            {
+                return new BadRequestObjectResult(new
+                {
+                    error = BadRequest("fill in all fields")
+                });
+            }
+            await _reader.UpdateReaderById(id,reader);
+            return Ok();
+        }
+
+        [HttpDelete("deleteReaderById/{id}")]
+        public async Task<IActionResult> DeleteReaderById(int id)
+        {
+
+            if (!_reader.GetAll().Any(r => r.Id_User == id))
+            {
+                return new NotFoundObjectResult(new
+                {
+                    error = NotFound("reader with that id does not exists")
+                });
+            }
+            await _reader.DeleteReaderById(id);
+            return Ok();
+        }
+
+        [HttpGet("getReaderById{id}")]
+        public async Task<IActionResult> GetReaderById(int id)
+        {
+
+            if (!_reader.GetAll().Any(r => r.Id_User == id))
+            {
+                return new NotFoundObjectResult(new
+                {
+                    error = NotFound("reader with that id does not exists")
+                });
+            }
+
+            return new OkObjectResult(new
+            {
+                reader = _reader.GetReaderById(id)
+            });
+        }
+        //[HttpGet("getReadersBooks/{id}")]
+        //public async Task<IActionResult> GetReadersRentals(int id)
+        //{
+
+        //    if (_rent.ReaderInRent(id))
+        //    {
+        //        return new NotFoundObjectResult(new
+        //        {
+        //            error = NotFound("reader has no rentals")
+        //        });
+        //    }
+        //    return new OkObjectResult(new
+        //    {
+        //        books = _reader.GetReadersRentals(id)
+        //    });
+        //}
+        [HttpGet("isAdmin")]
+        public async Task<IActionResult> checkRole()
+        {
+
+            return Ok("you admin");
+        }
+    }
+}
---
+++ ReadersServiceApi/Program.cs
@@ -0,0 +1,58 @@
+
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.EntityFrameworkCore;
+using ReadersServiceApi.dbContext;
+
+namespace ReadersServiceApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddHttpContextAccessor();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader();
+                    });
+            });
+            builder.Services.AddDbContext<ReadersApiDb>(options =>
+     options.UseSqlServer(builder.Configuration.GetConnectionString("TestDbString")), ServiceLifetime.Scoped);
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseCookiePolicy(new CookiePolicyOptions
+            {
+                MinimumSameSitePolicy = SameSiteMode.Strict,
+                HttpOnly = HttpOnlyPolicy.Always,
+                Secure = CookieSecurePolicy.Always
+            });
+
+            app.UseCors("AllowAllOrigins");
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
